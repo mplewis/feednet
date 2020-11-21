ba241a7888da17b7477a0bf6168c6c344c780cdf
Matt Lewis <matt@mplewis.com>
2020-11-21 15:45:21 -0700

    feat: recreate service

--- main.tf
+++ main.tf
@@ -108,6 +108,21 @@ resource "kubernetes_deployment" "podinfo" {
   }
 }
 
+resource "kubernetes_service" "podinfo" {
+  metadata {
+    name = "podinfo"
+  }
+  spec {
+    selector = {
+      app = "podinfo"
+    }
+    port {
+      port        = 80
+      target_port = 9898
+    }
+  }
+}
+
 resource "kubernetes_ingress" "podinfo" {
   metadata {
     name = "podinfo"
