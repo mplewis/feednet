38994b5f5257aa2333ac9b118bab24394e076168
Matt Lewis <matt@mplewis.com>
2020-11-21 20:06:49 -0700

    feat: install k8s alpha provider for manifests

--- versions.tf
+++ versions.tf
@@ -3,6 +3,11 @@ terraform {
     digitalocean = {
       source = "digitalocean/digitalocean"
     }
+
+    kubernetes-alpha = {
+      source  = "hashicorp/kubernetes-alpha"
+      version = "0.2.1"
+    }
   }
   required_version = ">= 0.13"
 }
