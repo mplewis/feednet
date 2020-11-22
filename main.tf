a714a47c82e478be3e63a9d46bbe8fa3491d4eb7
Matt Lewis <matt@mplewis.com>
2020-11-21 20:37:01 -0700

    feat: cluster issuer staging

--- main.tf
+++ main.tf
@@ -34,10 +34,18 @@ resource "digitalocean_kubernetes_cluster" "feednet" {
   }
 }
 
-provider "kubernetes-alpha" {
+provider "kubernetes" {
   load_config_file = false
   host             = digitalocean_kubernetes_cluster.feednet.endpoint
   token            = digitalocean_kubernetes_cluster.feednet.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.feednet.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "kubernetes-alpha" {
+  host  = digitalocean_kubernetes_cluster.feednet.endpoint
+  token = digitalocean_kubernetes_cluster.feednet.kube_config[0].token
   cluster_ca_certificate = base64decode(
     digitalocean_kubernetes_cluster.feednet.kube_config[0].cluster_ca_certificate
   )
@@ -80,6 +88,7 @@ resource "helm_release" "cert-manager" {
 }
 
 resource "kubernetes_manifest" "clusterissuer_letsencrypt_staging" {
+  provider = kubernetes-alpha
   manifest = {
     apiVersion = "cert-manager.io/v1"
     kind       = "ClusterIssuer"
