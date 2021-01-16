421d875320cc7a765ce31d505465ac0e8ccf6678
Matt Lewis <matt@mplewis.com>
2021-01-16 15:15:20 -0700

    feat: aegiseek

---
+++ deployments/aegiseek.tf
@@ -0,0 +1,52 @@
+resource "kubernetes_deployment" "aegiseek" {
+  metadata {
+    name = "aegiseek"
+    labels = {
+      app = "aegiseek"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "aegiseek"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "aegiseek"
+        }
+      }
+
+      spec {
+        container {
+          name  = "aegiseek"
+          image = "mplewis/aegiseek:1.0.0"
+          env {
+            name = "AUTH_TOKEN"
+            value_from {
+              secret_key_ref {
+                name = "aegiseek-bot-token"
+                key  = "aegiseek"
+              }
+            }
+          }
+          resources {
+            requests {
+              cpu    = "100m"
+              memory = "64Mi"
+            }
+            limits {
+              cpu    = "1"
+              memory = "256Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
